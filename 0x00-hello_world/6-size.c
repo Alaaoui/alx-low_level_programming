a91e55ef9a82f0ceeba104410c1e51834f542b8d
Alaaoui <elhalimael09@gmail.com>
2023-07-14 07:46:49 -0700

    last commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ printf("size of int %d byte(s)\n", sizeof(int));
 printf("size of long int %d byte(s)\n", sizeof(long int));
 printf("size of long long int %d byte(s)\n", sizeof(long long int));
 printf("size of float %d byte(s)\n", sizeof(float));
-    return (0);
+return (0);
 }
