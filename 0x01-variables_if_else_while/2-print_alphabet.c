273a5224d27c9dfb072ab9b6ee2481d85355c342
Alaaoui <elhalimael09@gmail.com>
2023-07-15 05:46:53 -0700

    forth commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <stdio.h>
+/**
+ *
+ * main - entry point
+ *
+ *
+ *Returns to 0
+ */
+int main(void){
+int lowerCase = 'a';
+while (lowerCase < 'z')
+{
+putchar (lowerCase);
+lowerCase += 1;
+}
+putchar('\n');
+return(0);
+}
