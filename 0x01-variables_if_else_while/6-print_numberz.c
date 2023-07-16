4b3490a076410c3a607e617ef8cbad2002037576
Alaaoui <elhalimael09@gmail.com>
2023-07-15 18:02:04 -0700

    new commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Returns to 0
+ */
+
+int main(void)
+{
+int num;
+for (num = 0; num <10; num++)
+putchar ((num % 10) + '0');
+putchar('\n');
+return (0);
+}
