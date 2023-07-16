f7795b0b843e0e0c52f5204f9fe72c0cec489a2c
Alaaoui <elhalimael09@gmail.com>
2023-07-15 17:39:04 -0700

    new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Returns to 0
+ */
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf('\n');
+return (0);
+}
