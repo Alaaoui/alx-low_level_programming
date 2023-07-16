4e2e73f4e85a8f6a4bae98c63a67af9056d25ae8
Alaaoui <elhalimael09@gmail.com>
2023-07-16 03:12:58 -0700

    wa akhiran

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 {
 int num;
 char lettre;
-for (num = 1; num < 10; num++)
+for (num = 0; num < 10; num++)
 putchar ((num % 10) + '0');
 for (lettre = 'a'; lettre <= 'f'; lettre++)
 putchar(lettre);
